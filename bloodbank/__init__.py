8fcf3a1c1f04131f24367010c71a5fa8966d0662
Mikhail Shkarubski <michaelshkarubski@gmail.com>
2023-12-04 16:02:48 +0300

    Prettify terminal I/O, add patient commands * Add new decorator for credentials verification * Add pretty ASCII messages * Add medical record review function

--- bloodbank/__init__.py
+++ bloodbank/__init__.py
@@ -1,9 +1,28 @@
+import os
+
 import psycopg2
+
+
+SYSTEM_ENTRY_MSG: str = """
+{padding} ▄▄▄▄    ██▓     ▒█████   ▒█████  ▓█████▄     ▄▄▄▄    ▄▄▄       ███▄    █  ██ ▄█▀
+{padding}▓█████▄ ▓██▒    ▒██▒  ██▒▒██▒  ██▒▒██▀ ██▌   ▓█████▄ ▒████▄     ██ ▀█   █  ██▄█▒ 
+{padding}▒██▒ ▄██▒██░    ▒██░  ██▒▒██░  ██▒░██   █▌   ▒██▒ ▄██▒██  ▀█▄  ▓██  ▀█ ██▒▓███▄░ 
+{padding}▒██░█▀  ▒██░    ▒██   ██░▒██   ██░░▓█▄   ▌   ▒██░█▀  ░██▄▄▄▄██ ▓██▒  ▐▌██▒▓██ █▄ 
+{padding}░▓█  ▀█▓░██████▒░ ████▓▒░░ ████▓▒░░▒████▓    ░▓█  ▀█▓ ▓█   ▓██▒▒██░   ▓██░▒██▒ █▄
+{padding}░▒▓███▀▒░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒▒▓  ▒    ░▒▓███▀▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ▒ ▒▒ ▓▒
+{padding}▒░▒   ░ ░ ░ ▒  ░  ░ ▒ ▒░   ░ ▒ ▒░  ░ ▒  ▒    ▒░▒   ░   ▒   ▒▒ ░░ ░░   ░ ▒░░ ░▒ ▒░
+{padding} ░    ░   ░ ░   ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░  ░     ░    ░   ░   ▒      ░   ░ ░ ░ ░░ ░ 
+{padding} ░          ░  ░    ░ ░      ░ ░     ░        ░            ░  ░         ░ ░  ░   
+{padding}      ░                            ░               ░                             
+"""
 
 
 class Terminal:
     def __init__(self, connection):
         self._connection = connection
+        columns, _ = os.get_terminal_size(0)
+
+        print(SYSTEM_ENTRY_MSG.format(padding=" " * ((columns - 81) // 2)))
 
     @property
     def connection(self):
--- bloodbank/user.py
+++ bloodbank/user.py
@@ -1,14 +1,31 @@
 import enum
+import os
 from collections import namedtuple
 
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
 
-from bloodbank import Terminal
+import bloodbank
+
+def _commit(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        args[0].term.connection.commit()
+
+    return wrapper
+
+def _requires_auth(func):
+    def wrapper(*args, **kwargs):
+        if args[0].status == UserStatus.GUEST:
+            raise PermissionError(f'Access denied to a guest user! Did you forget to authenticate?')
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
 
 
 class UserStatus(enum.Enum):
-    USER = 0
+    GUEST = 0
     PATIENT = 1
     STAFF = 2
     ADMIN = 3
@@ -17,44 +34,97 @@ class UserStatus(enum.Enum):
 class UserCommand(enum.Enum):
     AUTHENTICATE = 'g0'
 
+    GET_MEDICAL_RECORD = 'p0'
+
 
 UserCreds = namedtuple('UserCreds', 'email password status')
+MedicalRecord = namedtuple(
+    'MedicalRecord',
+    'record_id patient_id info birth_date height weight bmi'
+)
 
 
 class User:
     QUERY_MSG: str = """
-    ================================== BLOOD BANK HELP ==================================
-    User:
-        - Authenticate                                                                 g0
-    Staff:
-        - Create appointment                                                           s0
-        - Delete appointment                                                           s1
-        - Update appointment                                                           s2
-    Patient:
-        - Get your medical record                                                      p0
-    =====================================================================================
+    {padding} ▄▄ • ▄• ▄▌▄▄▄ ..▄▄ · ▄▄▄▄▄    Authenticate                                    g0
+    {padding}▐█ ▀ ▪█▪██▌▀▄.▀·▐█ ▀. •██   
+    {padding}▄█ ▀█▄█▌▐█▌▐▀▀▪▄▄▀▀▀█▄ ▐█.▪
+    {padding}▐█▄▪▐█▐█▄█▌▐█▄▄▌▐█▄▪▐█ ▐█▌·
+    {padding}·▀▀▀▀  ▀▀▀  ▀▀▀  ▀▀▀▀  ▀▀▀ 
+    
+    
+    {padding}.▄▄ · ▄▄▄▄▄ ▄▄▄· ·▄▄▄·▄▄▄      Create appointment                              s0
+    {padding}▐█ ▀. •██  ▐█ ▀█ ▐▄▄·▐▄▄·      View appointment                                s1
+    {padding}▄▀▀▀█▄ ▐█.▪▄█▀▀█ ██▪ ██▪       View appointments                               s2
+    {padding}▐█▄▪▐█ ▐█▌·▐█ ▪▐▌██▌.██▌.      Delete appointment                              s3
+    {padding} ▀▀▀▀  ▀▀▀  ▀  ▀ ▀▀▀ ▀▀▀       Update appointment                              s4
+    
+    
+    {padding} ▄▄▄· ▄▄▄· ▄▄▄▄▄▪  ▄▄▄ . ▐ ▄ ▄▄▄▄▄   Get medical record                        p0
+    {padding}▐█ ▄█▐█ ▀█ •██  ██ ▀▄.▀·•█▌▐█•██     View appointments                         p1
+    {padding} ██▀·▄█▀▀█  ▐█.▪▐█·▐▀▀▪▄▐█▐▐▌ ▐█.▪   
+    {padding}▐█▪·•▐█ ▪▐▌ ▐█▌·▐█▌▐█▄▄▌██▐█▌ ▐█▌·   
+    {padding}.▀    ▀  ▀  ▀▀▀ ▀▀▀ ▀▀▀ ▀▀ █▪ ▀▀▀    
+    
+    
     """
-    AUTH_MSG: str = "Enter user's email and password separated by space: "
-    WELCOME_MSG: str = "Welcome back, {email}!"
-    SIGN_UP_MSG: str = "Looks like there's no such user. Would you like to sign up? [y/*]"
+    AUTH_MSG: str = "\tEnter user's email and password separated by space: "
+    WELCOME_BACK_MSG: str = "\tWelcome back, {name}!"
+    WELCOME_MSG: str = "\tWelcome, {name}!"
+    SIGN_UP_MSG: str = "\tLooks like there's no such user. Would you like to sign up? [y/*]"
 
-    def __init__(self, terminal: Terminal):
+    def __init__(self, terminal: bloodbank.Terminal):
         self._email: str | None = None
         self._password: str | None = None
-        self._status: UserStatus = UserStatus.USER
-        self._terminal = terminal
+        self._status: UserStatus = UserStatus.GUEST
+        self._terminal: bloodbank.Terminal = terminal
 
-    @staticmethod
-    def commit(func):
-        def wrapper(*args, **kwargs):
-            func(*args, **kwargs)
-            args[0].term.connection.commit()
+        columns, _ = os.get_terminal_size(0)
 
-        return wrapper
+        self.QUERY_MSG = self.QUERY_MSG.format(padding=' ' * ((columns - 81) // 2 - 4))
 
     @property
-    def term(self):
+    def term(self) -> bloodbank.Terminal:
         return self._terminal
+
+    @property
+    def email(self) -> str:
+        return self._email
+
+    @property
+    def password(self) -> str:
+        return self._password
+
+    @property
+    def status(self) -> UserStatus:
+        return self._status
+
+    @_requires_auth
+    def full_name(self) -> str:
+        query: str = f"""
+        SELECT CONCAT(first_name, ' ', last_name) as full_name
+        FROM {'patient' if self.status == UserStatus.PATIENT else 'staff'}
+        WHERE email=%s AND password=%s
+        """
+
+        with self.term.connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute(query, (self.email, self.password))
+            response = cursor.fetchone()
+
+        return response.full_name
+
+    def id(self) -> int:
+        query: str = f"""
+        SELECT {'patient_id' if self.status == UserStatus.PATIENT else 'staff_id'}
+        FROM {'patient' if self.status == UserStatus.PATIENT else 'staff'}
+        WHERE email=%s AND password=%s
+        """
+
+        with self.term.connection.cursor() as cursor:
+            cursor.execute(query, (self.email, self.password))
+            response = cursor.fetchone()
+
+        return int(response[0])
 
     def interact(self):
         query: str = input(self.QUERY_MSG)
@@ -63,12 +133,15 @@ class User:
             response = input(self.AUTH_MSG).split()
 
             if self._authenticate(*response):
-                print(self.WELCOME_MSG.format(email=self._email))
+                print(self.WELCOME_BACK_MSG.format(name=self.full_name()))
             elif (
                     input(self.SIGN_UP_MSG) == 'y' and
                     self._sign_up(*input("Email, username, password, first_name, last_name, phone: ").split())
             ):
-                print("Hey, hi, hello, yo, what's up?")
+                print(self.WELCOME_MSG.format(name=self.full_name()))
+
+        elif query == UserCommand.GET_MEDICAL_RECORD.value:
+            self._get_medical_record()
 
     def _authenticate(self, email: str, password: str) -> bool:
         staff_query: str = """
@@ -95,9 +168,9 @@ class User:
                 self._password = patient_resp.password
                 self._status = UserStatus.PATIENT
 
-        return self._status is not UserStatus.USER
+        return self._status is not UserStatus.GUEST
 
-    @commit
+    @_commit
     def _sign_up(
             self, email: str, username: str | None,
             password: str, first_name: str,
@@ -126,3 +199,31 @@ class User:
         self._status = UserStatus.PATIENT
 
         return True
+
+    @_requires_auth
+    def _get_medical_record(self):
+        query: str = f"""
+        SELECT
+            health_card_id AS record_id, 
+            patient AS patient_id, 
+            description AS info, 
+            birth_date, 
+            height, 
+            weight,
+            bmi
+        FROM health_card
+        WHERE patient={self.id()}
+        """
+
+        with self.term.connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute(query)
+            response: MedicalRecord = cursor.fetchone()
+
+        print(f"""
+        RECORD ID:          {response.record_id}
+        PATIENT ID:         {response.patient_id}
+        INFO:               {response.info}
+        BIRTH DATE:         {response.birth_date}
+        WEIGHT:             {response.weight}
+        HEIGHT:             {response.height}
+        BODY MASS INDEX:    {response.bmi}""")
